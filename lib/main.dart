fa4bc451bc8cbceb5a63149870b20a4fdda062c0
Pranav Masekar <83063593+PranavMasekar@users.noreply.github.com>
2021-11-02 11:52:36 +0530

    Staggered animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'page animations/first_page.dart';
+import 'Staggered Animation/staggered_animation.dart';
+// import 'page animations/first_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FirstPage(),
+      home: StaggerAnimation(),
     );
   }
 }
